ceb60c98d116ace493d42601066d110a04cf0bd0
Malith Kalhara <malith.kalhara@randoli.ca>
2021-03-05 17:27:34 +0530

    Fixed compilation issue

--- src/main/java/ca/randoli/quickstart/springboot/helloservice/HelloServiceApplication.java
+++ src/main/java/ca/randoli/quickstart/springboot/helloservice/HelloServiceApplication.java
@@ -1,17 +1,17 @@
 package ca.randoli.quickstart.springboot.helloservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class HelloServiceApplication {
 
-	@Value("${greeting}")
-	private String greeting;
+	//@Value("${greeting}")
+	private String greeting="Hello from my local cluster";
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloServiceApplication.class, args);
